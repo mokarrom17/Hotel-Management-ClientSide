f2a23ca7179498891217204103506921bbc7db59
mokarrom17 <mokarrombasher@gmail.com>
2024-10-15 23:47:56 +0600

    added room details features

--- src/Pages/RoomDetails/RoomDetails.jsx
+++ src/Pages/RoomDetails/RoomDetails.jsx
@@ -1,9 +1,22 @@
 import { useLoaderData } from "react-router-dom";
 import hotelBg from "../../assets/HotelBg2.jpg";
+import { BsDoorClosed, BsDoorOpen } from "react-icons/bs";
+import { RiCheckDoubleFill } from "react-icons/ri";
 
 const RoomDetails = () => {
   const features = useLoaderData();
-  const { type, image, adults, child, price, description } = features;
+  const {
+    type,
+    image,
+    adults,
+    child,
+    price,
+    description,
+    checkInTime,
+    checkOutTime,
+    cancellationPolicy,
+    amenities,
+  } = features;
   return (
     <div>
       <div
@@ -57,6 +70,46 @@ const RoomDetails = () => {
             <div className="card-body text-black divide-y-2">
               <h2 className="card-title">Description</h2>
               <p>{description}</p>
+            </div>
+          </div>
+        </div>
+        {/*  Check-In Time & Checkout Time*/}
+        <div className="flex justify-center mt-6 shadow-md">
+          <div className="card bg-white w-[800px] shadow-xl">
+            <div className="card-body text-black divide-y-2">
+              <h2 className="card-title">Check-In Time & Checkout Time</h2>
+              <div className="flex">
+                <p className="flex gap-2">
+                  <BsDoorClosed className="text-xl"></BsDoorClosed> Check-In
+                  Time:{checkInTime}
+                </p>
+                <p className="flex gap-2">
+                  <BsDoorOpen className="text-xl"></BsDoorOpen> Checkout Time:
+                  {checkOutTime}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Cancellation Policy */}
+        <div className="flex justify-center mt-6 shadow-md">
+          <div className="card bg-white w-[800px] shadow-xl">
+            <div className="card-body text-black divide-y-2">
+              <h2 className="card-title text-2xl">Cancellation Policy</h2>
+              <p className="text-xl font-medium flex gap-2">
+                <RiCheckDoubleFill className="text-2xl"></RiCheckDoubleFill>{" "}
+                {cancellationPolicy}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center mt-6 shadow-md">
+          <div className="card bg-white w-[800px] shadow-xl">
+            <div className="card-body text-black divide-y-2">
+              <h2 className="card-title">Amenities</h2>
+              <div className="gap-4">
+                <p>{amenities}</p>
+              </div>
             </div>
           </div>
         </div>
